a55df473dfe33da868cfb5e329ea6cda1e20c8d7
Dineshkumar07dk <99134481+Dineshkumar07dk@users.noreply.github.com>
2023-05-26 12:14:36 +0530

    Update peering.tf

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc_peering_connection" "example" {
-  peer_vpc_id                     = vpc-03da39a4f5ea08dab
+resource "aws_vpc_peering_connection" "VPC-peering" {
+  peer_vpc_id                     = "vpc-03da39a4f5ea08dab"
   vpc_id                          = aws_vpc.kubernetes_vpc.id
   auto_accept                     = false
   allow_remote_vpc_dns_resolution = true
