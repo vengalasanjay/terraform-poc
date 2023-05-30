864be6936e72cef412594beda2acd38f3e43c10c
Dineshkumar07dk <99134481+Dineshkumar07dk@users.noreply.github.com>
2023-05-30 11:58:47 +0530

    Create prod.tfvars.tf

---
+++ prod.tfvars.tf
@@ -0,0 +1,8 @@
+# dev.tfvars
+aws_region            = "us-east-1"
+vpc_cidr_block        = "172.0.0.0/16"
+subnet_cidr_block     = "172.0.1.0/24"
+subnet_availability_zone = "us-east-1b"
+ami_id                = "ami-007855ac798b5175e"
+instance_type         = "t2.large"
+ssh_key_name          = "poc.keypair"
