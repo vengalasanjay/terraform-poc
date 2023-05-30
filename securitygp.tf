ec00b5e8f60ee43fb66eb1eb2b4da86124a0bb61
Dineshkumar07dk <99134481+Dineshkumar07dk@users.noreply.github.com>
2023-05-30 11:49:23 +0530

    Create securitygp.tf

---
+++ securitygp.tf
@@ -0,0 +1,22 @@
+# Create Security Group for Kubernetes nodes
+resource "aws_security_group" "kubernetes_sg" {
+  vpc_id = aws_vpc.kubernetes_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "kubernetes-security-group"
+  }
+}
