69c76b0a95bb1c3a8c47a8dc821528762a4d83f5
Dineshkumar07dk <99134481+Dineshkumar07dk@users.noreply.github.com>
2023-05-30 11:56:26 +0530

    Create dev.tfvars.tf

---
+++ dev.tfvars.tf
@@ -0,0 +1,8 @@
+# dev.tfvars
+aws_region            = "us-east-1"
+vpc_cidr_block        = "10.0.0.0/16"
+subnet_cidr_block     = "10.0.1.0/24"
+subnet_availability_zone = "us-east-1a"
+ami_id                = "ami-007855ac798b5175e"
+instance_type         = "t2.medium"
+ssh_key_name          = "poc.keypair"
