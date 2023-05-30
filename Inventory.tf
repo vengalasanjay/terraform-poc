f51b467e2b317006c26f8ba680d76cb10bff770d
vengalasanjay <112609021+vengalasanjay@users.noreply.github.com>
2023-05-30 18:11:57 +0530

    Update Inventory.tf

--- Inventory.tf
+++ Inventory.tf
@@ -8,9 +8,9 @@ resource "null_resource" "inventory_creation" {
   provisioner "local-exec" {
     command = <<-EOT
       sudo -S sh -c 'cat <<EOF > /etc/ansible/hosts
-      kubernetes-master ansible_host=${aws_instance.kubernetes_master.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/root/poc.keypair.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
-      kubernetes-node1 ansible_host=${aws_instance.kubernetes_node1.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/root/poc.keypair.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
-      kubernetes-node2 ansible_host=${aws_instance.kubernetes_node2.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/root/poc.keypair.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
+      kubernetes-master ansible_host=${aws_instance.kubernetes_master.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/root/k8-key.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
+      kubernetes-node1 ansible_host=${aws_instance.kubernetes_node1.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/root/k8-key.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
+      kubernetes-node2 ansible_host=${aws_instance.kubernetes_node2.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/root/k8-key.pem ansible_ssh_extra_args="-o StrictHostKeyChecking=accept-new"
       EOF'
     EOT
   }
